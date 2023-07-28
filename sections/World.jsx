cc7a0c0d09fb149e57734a7ca6bcac5aca63aa9c
0xcryptored <nalanarud@gmail.com>
2023-07-28 09:36:47 -0500

    fixed map on mobile

--- sections/World.jsx
+++ sections/World.jsx
@@ -24,14 +24,18 @@ const World = () => (
         variants={fadeIn("up", "tween", 0.3, 1)}
         className="relative mt-[68px] flex w-full h-[550px]"
       >
-        <img src="/map.png" alt="map" className="w-full h-full object-cover" />
-        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
-          <img src="people-01.png" alt="people" className="w-full h-full" />
+        <img src="/map.png" alt="map" className="md:w-full md:h-full h-2/3" />
+        <div className="absolute md:top-32 left-20 md:w-[70px] w-[5px] md:h-[70px] h-[5px] p-[6px] rounded-full bg-[#5d6680]">
+          <img
+            src="people-01.png"
+            alt="people"
+            className="w-full h-full"
+          />
         </div>
         <div className="absolute top-72 left-40 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
           <img src="people-02.png" alt="people" className="w-full h-full" />
         </div>
-        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+        <div className="absolute top-8 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
           <img src="people-03.png" alt="people" className="w-full h-full" />
         </div>
       </motion.div>
